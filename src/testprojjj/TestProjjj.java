df4a4315a952931062b56bc4f76ab0437efd95d6
amal <amal7omari17@gmail.com>
2018-11-08 22:36:25 +0000

    Notre  commit branche 2

--- src/testprojjj/TestProjjj.java
+++ src/testprojjj/TestProjjj.java
@@ -18,6 +18,7 @@ public class TestProjjj {
     
         System.out.println("Hello world");
         System.out.println("Hello world 2");
+        System.out.println("Hello world  Branche 2");
         
     }
     
